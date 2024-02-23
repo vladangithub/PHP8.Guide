422f0a034b821928ad971e487c04db0b6a7acf3b
azesm <azesm@yahoo.com>
2024-02-23 19:51:17 +0300

    PHP8.06_string_11_25

---
+++ 07_string/index.php
@@ -0,0 +1,11 @@
+<?php error_reporting(-1);
+
+$str = '<p>Hello 1</p>';
+$str2 = "<p>Hello 2</p>";
+
+//var_dump($str);
+
+echo "<p>Hello 1</p>\n";
+echo "<p>Hello \ 2</p>";
+echo PHP_EOL;
+echo "<p>Hello 3</p>\n";
