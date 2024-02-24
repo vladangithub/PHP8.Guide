aabe5233012529d29b961566eaafeb7cedc74a89
azesm <azesm@yahoo.com>
2024-02-25 01:15:39 +0300

    PHP8.10_Comparison Operators_Операторы сравнения

--- 10/index.php
+++ 10/index.php
@@ -4,27 +4,27 @@
 //$b = 3;
 //$a == $b;
 //
-//var_dump(2 == 3);
-//var_dump(3 == '3'); true
-//var_dump(3 === '3'); false
-//var_dump(2 != 3); true
-//var_dump(2 <> 3); true
-
-//var_dump(2 > 3); true
-//var_dump(2 >= 3); true
-//var_dump(2 < 3); false
-//var_dump(2 <= 3); false
-
-//var_dump(2 <=> 3); -1
-//var_dump(4 <=> 3); 1
-//var_dump(4 <=> 4); 0
-
-$a = 10;
-
-//var_dump($a++);
-//var_dump(++$a);
-
-//var_dump(--$a);
-//var_dump($a--);
-
-//var_dump($a);
+var_dump(2 == 3);
+////var_dump(3 == '3'); true
+////var_dump(3 === '3'); false
+////var_dump(2 != 3); true
+////var_dump(2 <> 3); true
+//
+////var_dump(2 > 3); true
+////var_dump(2 >= 3); true
+////var_dump(2 < 3); false
+////var_dump(2 <= 3); false
+//
+////var_dump(2 <=> 3); -1
+////var_dump(4 <=> 3); 1
+////var_dump(4 <=> 4); 0
+//
+//$a = 10;
+//
+////var_dump($a++);
+////var_dump(++$a);
+//
+////var_dump(--$a);
+////var_dump($a--);
+//
+////var_dump($a);
--- 11/index.php
+++ 11/index.php
@@ -1,2 +1,35 @@
-<?php
+<?php error_reporting(-1);
+
+
+//$q = 3 == 3 && 3 < 2; // true &&(И) false
+//var_dump($q); //false
+//
+//$w = 3 == 3 and 3 < 2; // true &&(И) false
+//var_dump($w); // true, так как у = приоритет выше, чем у and
+//
+//$e = 2 == 3 || 3 > 2; // false ||(ИЛИ) true
+//var_dump($e); //true
+//
+//$r = 2 == 3 or 3 > 2; // false or true
+//var_dump($r); // false
+
+//var_dump(2 > 3); // false
+//var_dump(!(2 > 3)); // true
+
+//echo "<p>Hello 1</p>" . PHP_EOL . "<p>Hello 2</p>" . PHP_EOL;
+
+//$name = 'Bobbb';
+//echo "Hello {$name}";
+//echo 'Hello ' . $name;
+
+$hi = 'Hello, ';
+//echo $hi = $hi . 'world!';
+echo $hi .= 'world!';
+
+
+
+
+
+
+
 
