c06a4e5fcf00f07e337ddad9d2fec7cdb1bd8b90
azesm <azesm@yahoo.com>
2024-02-26 23:09:14 +0300

    PHP8.12_Homework_1

---
+++ 13/index.php
@@ -0,0 +1,36 @@
+<?php error_reporting(-1);
+
+//echo 'Test 1 ', 'Test 2 ', 'Test 3';
+//print 'Test 1 ';
+
+$light = 'green';
+
+//if ($light == 'green')
+//    echo 'You can go';
+
+if ($light == 'green') {
+    echo 'You can go now';
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
