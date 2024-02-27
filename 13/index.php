05a76060db318b9c664a49013e8e7e7eb8ce37ed
azesm <azesm@yahoo.com>
2024-02-27 10:10:26 +0300

    PHP8.14_ternary operator

--- 13/index.php
+++ 13/index.php
@@ -3,14 +3,39 @@
 //echo 'Test 1 ', 'Test 2 ', 'Test 3';
 //print 'Test 1 ';
 
-$light = 'green';
+$light = 'red';
+
+if ($light == 'green') {
+    echo 'You can go now';
+} elseif ($light == 'yellow') {
+    echo 'Get ready';
+} elseif ($light == 'red') {
+    echo 'Stop';
+    if (2 > 3) {
+        echo '2 > 3';
+    } else {
+        echo '2 < 3';
+    }
+} else {
+    echo 'Run';
+}
 
 //if ($light == 'green')
 //    echo 'You can go';
 
-if ($light == 'green') {
-    echo 'You can go now';
-    }
+//if ($light == 'green') {
+//    echo 'You can go now';
+//} else {
+//    echo 'Stop';
+//}
+
+//if ($light == 'yellow') {
+//    echo 'Get ready';
+//}
+//
+//if ($light == 'red') {
+//    echo 'Stop';
+//}
 
 
 
