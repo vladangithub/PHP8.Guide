cf2252915b7b0f0c63ba7eb23b4a25ecd1de8a6c
azesm <azesm@yahoo.com>
2024-02-27 12:04:03 +0300

    PHP8.14_ternary operator

--- 14/index.php
+++ 14/index.php
@@ -1,1 +1,31 @@
-<?php
+<?php error_reporting(-1);
+
+$price = 10;
+//var_dump($price);
+
+$light = 'green1';
+
+//if ($light == 'green') {
+//    echo 'You can go';
+//} else {
+//    echo 'Stop';
+//}
+
+//echo ($light == 'green') ? 'You can go' : 'Stop';
+
+//echo $price ? $price : 'unknown price';
+//echo $price ?: 'unknown price';
+
+$test = $price>= 10 ?: 'unknown price';
+var_dump($test);
+
+
+
+
+
+
+
+
+
+
+
