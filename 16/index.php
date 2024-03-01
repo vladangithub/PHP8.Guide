3ed0f654d6bfa97afa0793b15a31fc5fb9306180
azesm <azesm@yahoo.com>
2024-03-01 20:51:19 +0300

    PHP8.16_Homework2

--- 16/index.php
+++ 16/index.php
@@ -53,8 +53,29 @@
 //    echo PHP_EOL;
 //}
 
+# 1.1 Homework
+//$year = 1900;
+//
+//echo "<select>";
+//while ($year <= 2021) {
+//    echo "<option value= '{$year}'>{$year}</option>";
+//    $year++;
+//}
+//echo "</select>";
 
-
+echo '<table border = "1" width="100%">';
+$tr = 1;
+while ($tr < 10) {
+    echo "<tr>";
+    $td = 1;
+    while ($td < 10) {
+        echo "<td> $td * $tr =" . $td * $tr . "</td>";
+        $td++;
+    }
+    echo "<tr>";
+    $tr++;
+}
+echo '</table>';
 
 
 
