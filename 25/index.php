7cd67db8a035ccf3e41695fa41f7864bc65a8f00
azesm <azesm@yahoo.com>
2024-03-11 12:59:50 +0300

    PHP8.25_Function

---
+++ 25/index.php
@@ -0,0 +1,127 @@
+<?php error_reporting(-1);
+
+//function sum($a, $b, ...$nums) {
+//    $res = $a * $b;
+//
+//    foreach ($nums as $num) {
+//        $res += $num;
+//    }
+//    echo $res;
+//};
+//
+//sum(1, 2, 3, 4, 5);
+
+//function test()
+//{
+//    static $a = 0;
+//    echo $a;
+//    $a++;
+//}
+//
+//test();
+//test();
+//test();
+//test();
+
+//function sum($a, $b,  ...$nums)
+//{
+//    $res = $a * $b;
+//
+//    foreach ($nums as $num) {
+//        $res += $num;
+//    }
+//    var_dump($res);
+//}
+//
+//sum(1, 2, 3, 4, 5);
+
+
+//function test()
+//{
+//    static $a = 0;
+//    echo $a;
+//    $a++;
+//
+//}
+//
+//test();
+//test();
+//test();
+//test();
+
+//function sum(int $a, int $b, int $c)
+//{
+//    echo $a + $b + $c;
+//}
+//
+//sum(1, 2.5, 3);
+
+//function test() {
+//
+//}
+//
+//var_dump(test());
+
+//function sum($a, $b, $c): float
+//{
+//    return $a + $b + $c;
+//}
+//
+//var_dump(sum(1, 2, 3));
+
+
+function sum($term1, $term2, $factor)
+{
+    return ($term1 + $term2) * $factor;
+}
+
+echo sum(1, 2, 3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
