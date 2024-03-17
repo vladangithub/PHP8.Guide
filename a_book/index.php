b4d436186b9ae8d59bdaf4625d365b627493fb1d
azesm <azesm@yahoo.com>
2024-03-17 16:09:57 +0300

    book 187-9.2

---
+++ a_book/index.php
@@ -0,0 +1,125 @@
+<?php
+/*$price = 3000;
+$price_vip = $price + 750;
+$price_vip = "неизвестно";
+echo "Price ticket: $price<br />";
+echo "Price VIP ticket: $price_vip<br />";
+
+$first = 1;
+$second = 2;
+echo $first + $second;
+
+$greeting = "Hello ";
+$subject = " PHP!";
+//echo $greeting + $subject;
+echo $greeting . $subject;
+
+echo 0755;
+echo 0o755;
+
+$str = 123;
+echo 'Значение переменной - $str';
+
+
+$text = "Паро";
+
+//echo "Едет {$text}воз <br /> ";
+//echo "Плывет {$text}ход <br /> ";
+
+echo "Едет $textвоз <br /> ";
+//echo "Плывет {$text}ход <br /> ";
+
+$str = "Hello, world!";
+echo "$str[10]$str[8]$str[1]$str[9]";
+
+
+$arr = [;
+    0 => "Нулевой элемент",;
+    "surname" => "Гетц",;
+    "name" => "Бобббб",;
+];
+echo $arr["surname"];
+$arr[1] = "Первый элемент";
+$arr["name"] = "Гога";
+
+echo '<pre>' . print_r($arr, 1) . '</pre>';
+
+
+if (rand(1 , 10) %2 == 0) echo"Выпало четное число";
+else echo"Выпало нечетное число";
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- a_book/variables5.php
+++
@@ -1,5 +0,0 @@
-<?php
-$price = 3000;
-$price_vip = $price + 750;
-echo "Price ticket: $price<br />";
-echo "Price VIP ticket: $price_vip<br />";
